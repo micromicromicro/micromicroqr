b5221a101358620b9c105595781d12d86a958dd4
micromicromicro <github@micromicro.cash>
2018-04-28 01:06:42 +0900

    Compiles

---
+++ build.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import subprocess
+import itertools
+import shutil
+import os
+import os.path
+import json
+
+
+if os.path.exists('build'):
+    shutil.rmtree('build')
+os.mkdir('build')
+
+# Package qr logo for svg embedding
+with open('qr_micro.svg', 'r') as source:
+    # skip <xml>
+    svgdata = ''.join(itertools.islice(source.readlines(), 1, None))
+with open('build/qr_micro.js', 'w') as out:
+    out.write('export const qrMicro = {};'.format(
+        json.dumps(svgdata)
+    ))
+
+# Package js and perform automatic additions
+subprocess.check_call(['./node_modules/.bin/tsc'])
---
+++ index.ts
@@ -0,0 +1,59 @@
+import { qrcode } from "qrcode-generator-es6";
+import { qrMicro } from "./qr_micro";
+
+export const genericSvgString = ({
+  low,
+  high,
+  addr,
+  obstructionData = null,
+  obstructionPath = null
+}: {
+  low: Array<number>;
+  high: Array<number>;
+  addr: string;
+  obstructionData?: string;
+  obstructionPath?: string;
+}): string => {
+  let qr = new qrcode(0, "H");
+  qr.addData(addr);
+  qr.make();
+  const obstruction = { width: 0.25, height: 0.25, svgData: null, path: null };
+  if (obstructionData) obstruction.svgData = obstructionData;
+  else if (obstructionPath) obstruction.path = obstructionPath;
+  const svg_data = qr.createSvgTag({
+    margin: 0,
+    cellColor: (c, r) => {
+      const modcount = qr.getModuleCount();
+      const interp =
+        (Math.pow(c - modcount / 2, 2) + Math.pow(r - modcount / 2, 2)) /
+        Math.pow(modcount, 2);
+      return (
+        "hsl(" +
+        (low[0] + (high[0] - low[0]) * interp).toString() +
+        ", " +
+        (low[1] + (high[1] - low[1]) * interp).toString() +
+        "%, " +
+        (low[2] + (high[2] - low[2]) * interp).toString() +
+        "%" +
+        ")"
+      );
+    },
+    obstruction: obstruction
+  });
+  return svg_data;
+};
+
+export const svgString = (addr: string): string => {
+  return genericSvgString({
+    addr: addr,
+    low: [119 / 255 * 360, 92 / 255 * 100, 114 / 255 * 100],
+    high: [152 / 255 * 360, 79 / 255 * 100, 70 / 255 * 100],
+    obstructionData: qrMicro
+  });
+};
+
+export const imgElement = (addr: string) => {
+  const img = document.createElement("img");
+  img.src = "data:image/svg+xml;utf-8," + svgString(addr);
+  img.alt = "Scan to pay with micromicro!";
+};
